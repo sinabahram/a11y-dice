e57fea9623ca7695180ffc531a7fe31660d4f299
Sina Bahram <sina@sinabahram.com>
2017-03-13 18:24:45 -0400

    trying ot get basic flow to work from index to roller

--- server/index.js
+++ server/index.js
@@ -1,4 +1,10 @@
+use strict;
+var port = 3000;
+
 var app = require('express')();
+var bodyParser = require('body-parser');
+// app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
@@ -6,14 +12,20 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.post('/', function(req, res){
+console.log('Got post request for player: '+req.body.playerName+' and room: '+req.body.roomName);
+res.redirect('/roller?name='+playerName+'&roomName='+roomName);
+});
+
+app.get('/roller', function(req, res){
+  res.sendFile(__dirname + '/roller.html');
+});
+
 io.on('connection', function(socket){
   console.log('a user connected');
 });
 
-
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:'+port);
 });
       
-
