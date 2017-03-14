b1726d52554504597f79b242d67fc71818c71059
Sina Bahram <sbahram@debian>
2017-03-14 01:12:05 -0400

    Basic end-to-end system works well. Now to implement rolling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 "use strict";
 var port = 3000;
 
+var players = {};
+
 var app = require('express')();
 var bodyParser = require('body-parser');
 // app.use(bodyParser.json()); // support json encoded bodies
@@ -18,7 +20,6 @@ app.post('/roller', function(req, res){
 var roomName = req.body.roomName,
 playerName = req.body.playerName;
 
-console.log('Got post request for /roller with player: '+playerName+' and room: '+roomName);
   res.render('roller', {
 roomName: roomName,
 playerName: playerName
@@ -26,8 +27,45 @@ playerName: playerName
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+socket.on('disconnect', function() { onDisconnect(socket);});
+socket.on('join', function(msg) { onJoin(socket, msg);});
+socket.on('command', function(msg) { onCommand(socket, msg);});
+
+var playerNames = [];
+for(var id in players) {
+playerNames.push(players[id].playerName);
+}
+
+console.log('sending player names: '+playerNames);
+socket.emit('players', {playerNames: playerNames});
 });
+
+function onDisconnect(socket) {
+console.log(players[socket.id].playerName+' left '+players[socket.id].roomName);
+io.sockets.in(players[socket.id].roomName).emit('leave', {playerName: players[socket.id].playerName});
+delete players[socket.id];
+};
+
+function onJoin(socket, msg) {
+console.log(msg.playerName+' joined '+msg.roomName);
+socket.join(msg.roomName);
+io.sockets.in(msg.roomName).emit('join', msg);
+
+players[socket.id] = {socket: socket};
+players[socket.id].playerName = msg.playerName;
+players[socket.id].roomName = msg.roomName;
+}
+
+function onCommand(socket, msg) {
+var playerName = msg.playerName,
+roomName = msg.roomName,
+command = msg.command;
+
+console.log('in '+roomName+': onCommand from '+playerName+': '+command);
+
+var result = "5";
+io.sockets.in(roomName).emit('results', {playerName: playerName, result: result});
+}
 
 http.listen(port, function(){
   console.log('listening on *:'+port);
